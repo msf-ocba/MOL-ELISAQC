7b665ae9d7e3a7d92b6ea9365090461616db7e5c
Amy Mikhail <amy.mikhail@barcelona.msf.org>
2021-03-09 17:19:44 +0100

    Added r markdown params to shiny app

--- ShinyApp/ELISAQCapp.R
+++ ShinyApp/ELISAQCapp.R
@@ -6,6 +6,7 @@
 library(shiny)
 library(rhandsontable)
 library(data.table)
+library(dplyr)
 
 
 ##########################################
@@ -43,15 +44,85 @@ controls <- c("Calibrateur",
               "Témoin négatif externe", 
               "Témoin positif externe") 
 
-
 ##########################################
 # DEFINE UI
 ##########################################
-
+    
 ui <- fluidPage(
     
-    # Create the handle for the empty table:
-    rHandsontableOutput(outputId = "platemap")
+    # Add drop-down menu of ELISA kits:
+    selectInput(inputId = "kit", 
+                label = "Kit ELISA", 
+                choices = c("Rougeole (IgM - NP)", 
+                            "Rubéole (IgM - GP)", 
+                           "Coqueluche (IgG - PT)"), 
+                 selected = "Rougeole (IgM - NP)", 
+                 multiple = FALSE), 
+
+    # Create an empty plate template for entering sample IDs:
+    rHandsontableOutput(outputId = "platemap"),
+    
+    # Add file up-loader for raw ELISA results:
+    fileInput(inputId = "resultats", 
+              label = "Résultats bruts du lecteur ELISA:", 
+              multiple = FALSE,
+              buttonLabel = "Rechercher...", 
+              placeholder = "Dossier pas encore selectionné"), 
+    
+    # Add file up-loader for reference lab control results:
+    fileInput(inputId = "refmaison", 
+              label = "Valeurs de référence pour controles de lab ref:",
+              multiple = FALSE, 
+              buttonLabel = "Rechercher...",
+              placeholder = "Dossier pas encore selectionné"),
+    
+    # Add input for ELISA operator:
+    textInput(inputId = "operateur", 
+              label = "Nom de l´operateur", 
+              value = "laborantin"), 
+    
+    # Add input for calibrator reference value:
+    numericInput(inputId = "calref", 
+                 label = "Calibrateur: valeur de référence", 
+                 value = 0.316), 
+    
+    # Add input for calibrator lower limit:
+    numericInput(inputId = "calinf", 
+                 label = "Calibrateur: limite inférieure", 
+                 value = 0.14),
+    
+    # Add input for internal positive control reference value:
+    numericInput(inputId = "ipcref", 
+                 label = "Controle positif du kit: valeur de référence", 
+                 value = 2.6),
+    
+    # Add input for internal positive control lower limit:
+    numericInput(inputId = "ipcinf", 
+                 label = "Controle positif du kit: limite inférieure", 
+                 value = 1.4),
+    
+    # Add input for internal positive control upper limit:
+    numericInput(inputId = "ipcsup", 
+                 label = "Controle positif du kit: limite supérieure", 
+                 value = 3.8),
+    
+    # Add input for internal negative control reference value:
+    numericInput(inputId = "incref", 
+                 label = "Controle négatif du kit: valeur de référence", 
+                 value = 0.1),
+    
+    # Add input for internal negative control lower limit:
+    numericInput(inputId = "incinf", 
+                 label = "Controle négatif du kit: limite inférieure", 
+                 value = 0),
+    
+    # Add input for internal negative control upper limit:
+    numericInput(inputId = "incsup", 
+                 label = "Controle négatif du kit: limite supérieure", 
+                 value = 0.7),
+
+    # Add report generator button:
+    downloadButton(outputId = "rapport", label = "Générer rapport CQ")
     
 )
 
@@ -80,6 +151,36 @@ server <- function(input, output){
         plate$values <- hot_to_r(input$platemap)
     })
     
+    output$report <- downloadHandler(filename = paste0("ELISAQCrapport_", Sys.Date(), ".html"), 
+                                     content = function(file) {
+                                         
+                                         # Copy file to temporary directory
+                                         tempReport <- file.path(tempdir(), "ELISAQCrapport.Rmd")
+                                         file.copy("ELISAQCrapport.Rmd", tempReport, overwrite = TRUE)
+                                         
+                                         # Set up parameters to pass to Rmd document
+                                         params <- list(kit = input$kit, 
+                                                        ids = plate$values, 
+                                                        resultats = input$resultats, 
+                                                        refmaison = input$refmaison, 
+                                                        operateur = input$operateur, 
+                                                        calref = input$calref, 
+                                                        calinf = input$calinf, 
+                                                        ipcref = input$ipcref, 
+                                                        ipcinf = input$ipcinf, 
+                                                        ipcsup = input$ipcsup, 
+                                                        incref = input$incref, 
+                                                        incinf = input$incinf, 
+                                                        incsup = input$incsup) 
+                                         
+                                         # Knit the document:
+                                         rmarkdown::render(input = tempReport, 
+                                                           output_file = paste0("ELISAQCrapport_", 
+                                                                                Sys.Date(), 
+                                                                                ".html"),
+                                                           params = params,
+                                                           envir = new.env(parent = globalenv()))
+                                     })
 }
 
 ##########################################
